b3633e09040fcdfe31cd477d6bdc0b77a43bee53
Em01 <emma_williams1@hotmail.co.uk>
2015-01-25 20:48:05 +0000

    initial commit

---
+++ spec/game_of_life_spec.rb
@@ -0,0 +1,8 @@
+require 'rspec'
+
+describe 'game of life' do 
+	it "Rule #1: Any live cell with fewer than two live neighbours dies, as if caused by under population" do 
+		cell = Cell.new
+		cell.neighbours_count.should == 0
+	end
+end
