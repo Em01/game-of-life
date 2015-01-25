0b756455468dee9e5ca0a6e4e68d9e083fff6596
Em01 <emma_williams1@hotmail.co.uk>
2015-01-25 20:58:46 +0000

    initial test passes but needs further work

--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -1,9 +1,9 @@
 require 'rspec'
-
+require 'cell'
 
 describe 'game of life' do 
 	it "Rule #1: Any live cell with fewer than two live neighbours dies, as if caused by under population" do 
 		cell = Cell.new
-		cell.neighbours_count.should == 0
+		expect(cell.neighbours.count).to be == 0
 	end
 end
