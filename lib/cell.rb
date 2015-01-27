ef9b1051b4a94ee81bbbf1c9cc4fc9945ad806e5
Em01 <emma_williams1@hotmail.co.uk>
2015-01-27 12:49:50 +0000

    defined accessor method for x and y

--- lib/cell.rb
+++ lib/cell.rb
@@ -1,5 +1,11 @@
 class Cell
+	attr_accessor :x, :y
+	
 	def neighbours
 		[]
+	end
+
+	def spawn_at(x, y)
+		Cell.new
 	end
 end
--- spec/game_of_life_spec.rb
+++ spec/game_of_life_spec.rb
@@ -2,6 +2,16 @@ require 'rspec'
 require 'cell'
 
 describe 'game of life' do 
+	context "cell utility methods" do 
+		subject { Cell.new }
+		it "spawn relative to" do 
+			cell = subject.spawn_at(3,5)
+			expect(cell.is_a?(Cell)).to be_true
+			expect(cell.x).to == 3
+			expect(cell.y).to == 5
+		end
+	end
+
 	it "Rule #1: Any live cell with fewer than two live neighbours dies, as if caused by under population" do 
 		cell = Cell.new
 		expect(cell.neighbours.count).to be == 0
